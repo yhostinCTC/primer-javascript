be1111290852c03fbe63b93ee37439b0ea1366ab
Yhostin-ctc <yollachica@crackthecode.la>
2022-11-08 20:13:58 -0500

    Change javascript: Add variables and operations

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,43 @@
 
-
 // Variables
+// Condicionales
+// Bucles
 
-let x = 3;
 
 // imprimir nuestra variable
-console.log(x)
+
+
+
+
+// Creen variables para su nombre, apellido, edad, altura y peso.
+// Imprimir cada una de estas variables
+let nombre = "Yhostin marcos";
+let edad = 24;
+let altura = 1.69; // esta en metros
+
+
+console.log(nombre)
+console.log(edad)
+console.log(altura)
+
+
+// Operaciones matematicas
+let a = 50
+let b = 30
+let c = 70
+
+let resultado1 = 0
+let resultado2 = 0
+let resultado3 = 0
+let resultado4 = 0 
+
+resultado1 = 
+resultado2 = 
+resultado3 = 
+resultado4 = 
+
+console.log(resultado1)
+
+
+// Creen 4 operaciones combinadas usando los numeros, variables y
+// operaciones que deseen
